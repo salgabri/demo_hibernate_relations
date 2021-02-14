b4dedde5e713c9814caf1a2d1e39eaed5d622032
Salvatore Gabrieli <salgabri@gmail.com>
2021-02-14 17:06:40 +0100

    child otm entity

--- src/main/java/com/example/demo/entities/ChildOTM.java
+++ src/main/java/com/example/demo/entities/ChildOTM.java
@@ -1,4 +1,19 @@
 package com.example.demo.entities;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "child_otm")
+@Data
 public class ChildOTM {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
 }
